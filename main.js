7d760a1a1e5ce837231947e1fa094e0f00b2cb91
T0mmmyy <tommilevis6@gmail.com>
2023-11-04 22:27:55 +0500

    Lesson five math

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-var num = 5;
-num = 7;
-console.log("Переменная: " + num);
-number = true;
-var num_1 = 5;
-var num_1 = "5";
+var num_1 = 10;
+var num_2 = 15;
+
+console.log("Результат: " + (num_1 + num_2));
+var num_3 = 5;
+num_3 = num_3 + 7
+console.log("Результат: " + num_3);
+
+var str_1 = Number('12');
+var str_2 = Number('2');
+console.log('Результат: ' + (str_1 +str_2));
+console.log("Math: " + Math.PI)
+console.log("Math: " + Math.min(4, 6, 5, 7, 2))
+console.log("Math: " + Math.max(4, 6, 5, 7, 2))
