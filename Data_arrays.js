05677351afd1cfa0b461dc3ebe292aeb5267f989
T0mmmyy <tommilevis6@gmail.com>
2023-11-05 23:31:27 +0500

    Lesson seven data sets

---
+++ Data_arrays.js
@@ -0,0 +1,9 @@
+var arr = [5, true, "stroka", 5.7, 0, -100];
+arr[3] = "word";
+console.log(arr.length);
+var martix = [
+    [4, 6, 8], ["stroka", 5.7], [0, -100]
+];
+
+martix[1] [0] = 90;
+console.log(martix)
