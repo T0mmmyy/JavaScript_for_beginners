3c10872103969fd2417721b7b89cb7abbd3cf98a
T0mmmyy <tommilevis6@gmail.com>
2023-11-07 22:21:32 +0500

    Lesson Eight Loop Operators

---
+++ Loop Operators.js
@@ -0,0 +1,19 @@
+for(var i = 100; i >= 25; i /= 2) {
+    console.log(i);
+}
+var j = 1000;
+while(j >= 100) {
+    console.log(j);
+    j -= 100
+}
+
+//var ishascar = true;
+//while(ishascar) {
+
+//}
+
+var x = 0;
+do {
+    console.log(x);
+    x++;
+} while(x < 10);
