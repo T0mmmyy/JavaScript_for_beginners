582da5ce05a16b63967f76b805c4af32b163f7d7
T0mmmyy <tommilevis6@gmail.com>
2023-11-13 22:59:06 +0500

    Lesson twelve Events and event handler

--- Events and event handler.js
+++ Events and event handler.js
@@ -3,8 +3,11 @@ var num = 0
 function onClickButton(el) {
     num++;
     el.innerHTML = "Вы нажали на кнопку:  " + num;
+    el.style.cssText = "border-radius: 20px; border: 0; font-size: 20px;"
 }
 
 function onInput(el) {
+    if(el.value == "Hello")
+        alert("И тебе привет")
     console.log(el.value)
 }
