2a2aafd10527c578ed95d9fcb31a4178a1055a1c
T0mmmyy <tommilevis6@gmail.com>
2023-11-16 00:11:30 +0500

    Managing HTML and Form Processing with JS

--- form processing.js
+++ form processing.js
@@ -1,14 +1,16 @@
-var text = document.getElementById('text')
-text.id = "New text";
-console.log(text.id);
+// var text = document.getElementById('text')
+// text.id = "New text";
+// console.log(text.id);
 
-text.style.color = "red";
-text.style.backgroundColor = "black";
+// text.style.color = "red";
+// text.style.backgroundColor = "black";
 
-text.innerHTML = "New<br>string";
+// text.innerHTML = "New<br>string";
 
-var spans = document.getElementsByTagName('span');
+// var spans = document.getElementsByTagName('span');
 
-for(var i = 0; i < spans.length; i++) {
-    console.log(spans[i].innerHTML);
-}
+// var spans = document.getElementsByClassName('simple-text');
+
+// for(var i = 0; i < spans.length; i++) {
+//     console.log(spans[i].innerHTML);
+// }
