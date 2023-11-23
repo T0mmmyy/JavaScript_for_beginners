e2da680afda72b7f5d90d6e351c4c23e7d0272ad
T0mmmyy <tommilevis6@gmail.com>
2023-11-23 22:45:31 +0500

    Creating timers and intervals

---
+++ Creating_timers_and_intervals.js
@@ -0,0 +1,17 @@
+// var counter = 0;
+// var id = setInterval(my_func, 1000);
+// function my_func() {
+//     counter++;
+//     console.log("Counter: " + counter);
+
+//     if(counter == 3)
+//         clearInterval(id);
+// }
+// setInterval(function() {
+//     counter++;
+//     console.lgo("Прошло: " + counter);
+// }, 1000);
+
+setTimeout(function() {
+    console.log("Taimer is working")
+}, 2000)
